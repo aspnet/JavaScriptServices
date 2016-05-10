0571e4ea831d184327bda7b750c317937ffc6bf9
Andrei Tserakhau <andrei_tserakhau@epam.com>
2016-05-10 14:51:59 +0300

    bug(ng2-template): fix issue with not disposed boot object

--- templates/Angular2Spa/ClientApp/boot-server.ts
+++ templates/Angular2Spa/ClientApp/boot-server.ts
@@ -6,24 +6,27 @@ import { BASE_URL, ORIGIN_URL, REQUEST_URL } from 'angular2-universal/common';
 import { App } from './components/app/app';
 
 export default function (params: any): Promise<{ html: string, globals?: any }> {
-    const serverBindings = [
-        ngCore.provide(BASE_URL, { useValue: '/' }),
-        ngCore.provide(ORIGIN_URL, { useValue: params.origin }),
-        ngCore.provide(REQUEST_URL, { useValue: params.url }),
-        ...ngUniversal.NODE_PLATFORM_PIPES,
-        ...ngUniversal.NODE_ROUTER_PROVIDERS,
-        ...ngUniversal.NODE_HTTP_PROVIDERS,
-    ];
+  const serverBindings = [
+    ngCore.provide(BASE_URL, { useValue: '/' }),
+    ngCore.provide(ORIGIN_URL, { useValue: params.origin }),
+    ngCore.provide(REQUEST_URL, { useValue: params.url }),
+    ...ngUniversal.NODE_PLATFORM_PIPES,
+    ...ngUniversal.NODE_ROUTER_PROVIDERS,
+    ...ngUniversal.NODE_HTTP_PROVIDERS,
+  ];
 
-    return ngUniversal.bootloader({
-        directives: [App],
-        componentProviders: serverBindings,
-        async: true,
-        preboot: false,
-        // TODO: Render just the <app> component instead of wrapping it inside an extra HTML document
-        // Waiting on https://github.com/angular/universal/issues/347
-        template: '<!DOCTYPE html>\n<html><head></head><body><app></app></body></html>'
-    }).serializeApplication().then(html => {
-        return { html };
-    });
+  let boot = ngUniversal.bootloader({
+    directives: [App],
+    componentProviders: serverBindings,
+    async: true,
+    preboot: false,
+    // TODO: Render just the <app> component instead of wrapping it inside an extra HTML document
+    // Waiting on https://github.com/angular/universal/issues/347
+    template: '<!DOCTYPE html>\n<html><head></head><body><app></app></body></html>'
+  })
+
+  return boot.serializeApplication().then(html => {
+    boot.dispose();
+    return { html };
+  });
 }
