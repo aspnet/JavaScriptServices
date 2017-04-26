7fcb6a2d7cae7a1482b487859c9adb7f239cab7d
Shawn Doucet <shawndoucet@azuresoft.net>
2017-04-26 17:50:28 -0500

    enable hot module reload

--- templates/AureliaSpa/webpack.config.js
+++ templates/AureliaSpa/webpack.config.js
@@ -1,12 +1,19 @@
 var isDevBuild = process.argv.indexOf('--env.prod') < 0;
 var path = require('path');
 var webpack = require('webpack');
-var AureliaWebpackPlugin = require('aurelia-webpack-plugin');
-
+const { AureliaPlugin } = require("aurelia-webpack-plugin");
+console.log("isDevBuild=" + isDevBuild);
 var bundleOutputDir = './wwwroot/dist';
 module.exports = {
-    resolve: { extensions: [ '.js', '.ts' ] },
-    entry: { 'app': 'aurelia-bootstrapper-webpack' }, // Note: The aurelia-webpack-plugin will add your app's modules to this bundle automatically
+    resolve: {
+        extensions: ['.js', '.ts'],
+        modules: ['ClientApp','node_modules']
+    },
+    entry: {
+        'app': [
+            'webpack-hot-middleware/client?reload=true',
+            'aurelia-bootstrapper']
+    }, // Note: The aurelia-webpack-plugin will add your app's modules to this bundle automatically
     output: {
         path: path.resolve(bundleOutputDir),
         publicPath: '/dist',
@@ -16,22 +23,30 @@ module.exports = {
         loaders: [
             { test: /\.ts$/, include: /ClientApp/, loader: 'ts-loader', query: { silent: true } },
             { test: /\.html$/, loader: 'html-loader' },
-            { test: /\.css$/, loaders: [ 'style-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize' ] },
+            { test: /\.css$/, loader: 'raw-loader' },
             { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
             { test: /\.json$/, loader: 'json-loader' }
         ]
     },
     plugins: [
+        new AureliaPlugin({
+            //https://github.com/jods4/aurelia-webpack-build/wiki/AureliaPlugin%20options
+            root: path.resolve('./'),
+            includeAll: 'ClientApp',
+            baseUrl: '/',
+            aureliaApp: "boot",
+            features: {
+                ie:  true,
+                svg: false,
+                unparser:  true              
+            }
+        }),
         new webpack.DefinePlugin({ IS_DEV_BUILD: JSON.stringify(isDevBuild) }),
         new webpack.DllReferencePlugin({
             context: __dirname,
             manifest: require('./wwwroot/dist/vendor-manifest.json')
         }),
-        new AureliaWebpackPlugin({
-            root: path.resolve('./'),
-            src: path.resolve('./ClientApp'),
-            baseUrl: '/'
-        })
+
     ].concat(isDevBuild ? [
         // Plugins that apply in development builds only
         new webpack.SourceMapDevToolPlugin({
