ff4a084a89fc86c1f151bdb494e870c479f0b370
bm-software <bmsoftware.co@gmail.com>
2017-05-30 09:55:06 +0200

    Update app.component.ts
    Resolve Angular and Bootstrap issues (https://github.com/aspnet/JavaScriptServices/issues/960)

--- templates/AngularSpa/ClientApp/app/components/app/app.component.ts
+++ templates/AngularSpa/ClientApp/app/components/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+// Workaround for issue https://github.com/aspnet/JavaScriptServices/issues/960
+import 'jquery';
+import 'bootstrap';
+
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
