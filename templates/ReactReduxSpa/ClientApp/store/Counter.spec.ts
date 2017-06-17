1a6d5ea10526e0d16811d9406aa39dae0de309b3
Sławomir Rosiek <rosieks@hotmail.com>
2017-06-17 13:41:24 +0200

    Added sample reducer tests for ReactRedux template

---
+++ templates/ReactReduxSpa/ClientApp/store/Counter.spec.ts
@@ -0,0 +1,22 @@
+﻿import 'jest';
+import { reducer } from './Counter';
+
+describe('Coutner reducer', () => {
+    it('should handle initial state', () => {
+        expect(reducer(undefined, {} as any)).toEqual({ count: 0 });
+    });
+
+    it('should handle INCREMENT_COUNT', () => {
+        expect(reducer(
+            { count: 0 },
+            { type: 'INCREMENT_COUNT' }))
+        .toEqual({ count: 1 });
+    });
+
+    it('should handle DECREMENT_COUNT', () => {
+        expect(reducer(
+            { count: 1 },
+            { type: 'DECREMENT_COUNT' }))
+            .toEqual({ count: 0 });
+    });
+});
